f5f54e2ee6a48fb1f203ee7f7932577c684a2222
a-nuc <anaranju@gmail.com>
2021-10-19 16:37:19 -0500

    Anotaciones

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -1,14 +1,23 @@
-using System.Collections.Generic;  
-using System.Linq; 
-using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Dominio
 {
     public class Cliente : Usuario
     {
-        public string Email {get; set;}
-        public string Direccion {get; set;}
-        public string Telefono {get; set;}
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido.")]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido.")]
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
 
         /*public Cliente(string Nombre, string Apellido, string Cedula, string Email, string Direccion, string Telefono, string User, string Pass):base( Nombre, Apellido, Cedula, User, Pass)
         {
